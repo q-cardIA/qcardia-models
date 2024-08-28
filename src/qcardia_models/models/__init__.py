c2c014d20a316f92e3231f41b07a3b57d1cd7de5
RobdeMooij <45096223+RobdeMooij@users.noreply.github.com>
2024-08-28 09:44:11 +0200

    Clean up available model imports

--- src/qcardia_models/models/__init__.py
+++ src/qcardia_models/models/__init__.py
@@ -1,9 +1,4 @@
-__all__ = [
-    "networks",
-    "EncoderMLP2d",
-    "UNet2d",
-]
+__all__ = ["EncoderMLP2d", "UNet2d"]
 
-from qcardia_models.models import networks
 from qcardia_models.models.networks.encoder_mlp import EncoderMLP2d
 from qcardia_models.models.networks.u_net2d import UNet2d
